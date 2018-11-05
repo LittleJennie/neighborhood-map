ad5ca137b7d74cd1220f5b22affbf324f6e93b2b
Jennie Zeng <jenniezeng1@gmail.com>
2018-11-04 18:18:38 -0800

    Four Square API fetched and add to state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
+
+function loadGoogleMapScript(url) {
+  const index = window.document.getElementsByTagName('script')[0];
+  const script = window.document.createElement('script');
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index)
+}
+
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount () {
+    this.getVenue()
     this.renderMap()
   }
 
   renderMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=AIzaSyBJhg8V9CYH7XONDGiFh-s7hnDqZxNScmE&v=3&callback=initMap"
+    loadGoogleMapScript("https://maps.googleapis.com/maps/api/js?libraries=geometry,drawing&key=AIzaSyBJhg8V9CYH7XONDGiFh-s7hnDqZxNScmE&v=3&callback=initMap"
     )
     window.initMap = this.initMap;
   }
+
+  getVenue = (() => {
+    const fsVenueRecEndPoint = '//api.foursquare.com/v2/venues/explore?'
+    const param = {
+      client_id: 'VJ4TVWJKEV4ED41VJ4GLAEEDCLYSST4J35DMEP4FCDCPD1G4',
+      client_secret: 'ZUAMLAFVTGPRWPLO0MVEM3OUW3LS2M00KVR5FJGCWXQZ5JLA',
+      query: 'food',
+      near: 'Sydney',
+      v: "20182507"
+    }
+    axios.get(fsVenueRecEndPoint + new URLSearchParams(param))
+      .then((res) => {
+        this.setState({venues: res.data.response.groups[0].items})
+      })
+      .catch((error) => {
+        console.log("Error! " + error)
+      })
+
+  })
 
   initMap = (() => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: -34.397, lng: 150.644},
       zoom: 8
-    });
+    })
   })
 
   render() {
@@ -31,14 +65,6 @@ class App extends Component {
   }
 }
 
-function loadScript(url) {
-  const index = window.document.getElementsByTagName('script')[0];
-  const script = window.document.createElement('script');
-  script.src = url;
-  script.async = true;
-  script.defer = true;
-  index.parentNode.insertBefore(script, index)
-}
 
 
 export default App;
