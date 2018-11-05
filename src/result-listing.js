a3639aef843bfdbc11139e7ec75629fb1e1f21dc
Jennie Zeng <jenniezeng1@gmail.com>
2018-11-04 22:25:08 -0800

    css updated

--- src/result-listing.js
+++ src/result-listing.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class ResultListing extends Component {
   render() {
     return (
-			<div id='listing'>Holo</div>
+			<div id='listing'>
+				<h1 className='listing-title'>SF Chinese Food</h1>
+			</div>
 		)
   }
 }
